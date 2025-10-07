da9a1dfa47bad2c934d6a8c656ad74918427a2fd
James Brunet <jamesbrunet@cunet.carleton.ca>
2025-10-07 08:24:06 -0400

    Modify template and fetch data from database

--- todoapp/models.py
+++ todoapp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Task(models.Model):
     description = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Task Object: {self.description} ({self.completed}) <ID: {self.pk}>"
--- todoapp/views.py
+++ todoapp/views.py
@@ -4,4 +4,8 @@ from .models import Task
 # Create your views here.
 def index(request):
     tasks = Task.objects.all()
+
+    for task in tasks:
+        task.completed = not task.completed
+        task.save()
     return render(request, 'index.html', context={'tasks':tasks})
