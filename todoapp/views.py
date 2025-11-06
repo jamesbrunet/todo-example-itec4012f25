fc1c8e5d32f26cc8017cb5dd464b5ff84197cbed
James Brunet <jamesbrunet@cunet.carleton.ca>
2025-11-06 09:57:51 -0500

    allow CORS from react app

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,13 +3,17 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import TaskSerializer
 from .models import Task
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import AllowAny
+import time
 
-# Create your views here.
 @api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
 def task_list(request):
     if request.method == 'GET':
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
+        time.sleep(1)
         return Response(serializer.data)
     if request.method == 'POST':
         serializer = TaskSerializer(data=request.data)
--- todoproject/settings.py
+++ todoproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'todoapp',
 ]
 
@@ -55,11 +56,17 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
 ]
 
 ROOT_URLCONF = 'todoproject.urls'
